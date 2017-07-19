c3c0bba174ddb83862c8f8a697a6dbe5cc300e9c
Daryna Buzovska <buzovskadaryna@gmail.com>
2017-07-19 10:43:21 +0300

    creates new method

--- src/com/company/Facebook.java
+++ src/com/company/Facebook.java
@@ -3,40 +3,35 @@ package com.company;
 class Facebook {
 
     static String compressNumber(int number) {
-        System.out.print(number + " => ");
         if (number < 1000) {
-            return ""+ number;
+            return "" + number;
         } else if (number >= 1000 && number < 10000) {
-            if (Math.floor(number/100)%10 == 0) {
-                return (int)Math.floor(number/1000) + "K";
-            } else {
-                return Math.floor(number/100)/10 + "K";
-            }
+            return getStringNumber(number,1000,"K");
         } else if (number >=10000 && number < 1000000) {
             return (int)Math.floor(number/1000) + "K";
-        }else if (number >=1000000 && number < 1000000000) {
-            if (Math.floor(number / 100000) % 10 == 0) {
-                return (int) Math.floor(number / 1000000) + "M";
-            } else {
-                return Math.floor(number / 100000) / 10 + "M";
-            }
-        }else{
-            if (Math.floor(number/100000000)%10 == 0) {
-                return (int)Math.floor(number/1000000000) + "B";
-            } else {
-                return Math.floor(number/100000000)/10 + "B";
-            }
+        } else if (number >=1000000 && number < 1000000000) {
+            return getStringNumber(number, 1000000, "M");
+        } else{
+            return getStringNumber(number, 100000000, "B");
+        }
+    }
+
+    private static String getStringNumber(int number, int order, String letter) {
+        if (Math.floor(number/(order/10))%10 == 0) {
+            return (int)Math.floor(number/(order/10))/10 + letter;
+        } else {
+            return Math.floor(number/(order/10))/10 + letter;
         }
     }
 
     static String toOrdinary(int number) {
         int lastItem = number%10;
-        int lastTwoItemes = number%100;
-        if (lastItem == 1 && lastTwoItemes != 11) {
+        int lastTwoItems = number%100;
+        if (lastItem == 1 && lastTwoItems != 11) {
             return number + "st";
-        } else if (lastItem == 2 && lastTwoItemes != 12) {
+        } else if (lastItem == 2 && lastTwoItems != 12) {
             return number + "nd";
-        } else if (lastItem == 3 && lastTwoItemes != 13) {
+        } else if (lastItem == 3 && lastTwoItems != 13) {
             return number + "rd";
         } else {
             return number + "th";
